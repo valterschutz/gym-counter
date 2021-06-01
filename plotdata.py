35ceb322814424d50d9a669698647266edb5bd57
Valter Schütz <valterschutz@protonmail.com>
2021-06-01 19:19:56 +0200

    Use seaborn theme

--- plotdata.py
+++ plotdata.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 import pandas as pd
+sns.set_theme()
 
 date = input("Choose a date to plot: ")
 df = pd.read_csv(f"data/{date.strip()}.csv", names=['time', 'weekday', 'count'])
