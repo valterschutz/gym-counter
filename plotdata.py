25a771425db241afb84e1439a299d5d2027926b2
Valter Schütz <valterschutz@protonmail.com>
2021-06-02 14:21:55 +0200

    Plot hours on x-axis

--- plotdata.py
+++ plotdata.py
@@ -1,9 +1,21 @@
+import os
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 import pandas as pd
 sns.set_theme()
 
-date = input("Choose a date to plot: ")
-df = pd.read_csv(f"data/{date.strip()}.csv", names=['time', 'weekday', 'count'])
+date = input("Choose a date to plot: ").strip()
 
-df.plot(x='time',y='count')
+if not os.path.exists(f"data/{date}.csv"):
+    print(f'The file "data/{date}.csv" does not exist.')
+
+df = pd.read_csv(f"data/{date}.csv", names=['time', 'weekday', 'count'])
+
+df['time'] = pd.to_datetime(df['time'])
+
+title = date + ' ' + df['weekday'][0]
+df.plot(x='time',y='count', title=title)
+time_format = mpl.dates.DateFormatter('%H')
+plt.gca().xaxis.set_major_formatter(time_format)
+plt.show()
